73385d5d9faf4cd3a28c7512117d535e281ade4d
Mimi Kim <mimikimwebdeveloper@gmail.com>
2016-10-06 02:50:34 -0400

    creating custom json, adding php

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 This web app finds and maps the location of every Baltimore city public library listed on the Enoch Pratt Library website.
 
-It makes use of a JSON data set located at [https://data.baltimorecity.gov/Culture-Arts/Libraries/tgtv-wr5u](https://data.baltimorecity.gov/Culture-Arts/Libraries/tgtv-wr5u) and utilizes Google Maps Javascript API.
+It makes use of a JSON data set located at  and utilizes Google Maps Javascript API.
+
+This was inspired by a public JSON data set located at [https://data.baltimorecity.gov/Culture-Arts/Libraries/tgtv-wr5u](https://data.baltimorecity.gov/Culture-Arts/Libraries/tgtv-wr5u). Unfortunately the file was incomplete and could have been organized better, so I went through the Pratt Library website to collect data and created my own JSON file to reference.
--- index.php
+++ index.php
@@ -4,37 +4,59 @@
     <title>Baltimore Library Locations</title>
     <script src="jquery-3.1.1.min.js"></script>
 </head>
+<style>
+    body {
+        padding-bottom: 50px;
+    }
+
+    #map {
+        width: 100%;
+        height: 400px;
+        margin-bottom: 30px;
+    }
+
+    .location {
+        margin: 15px 0;
+    }
+
+    @media screen and ( min-width: 768px ) {
+        #map {
+            width: 400px;
+        }
+    }
+</style>
 <body>
 <h1>Baltimore's Enoch Pratt Free Library Locations</h1>
 <p>
-    This web app finds and maps the location of every Baltimore city public library listed on the Enoch Pratt Library website. It makes use of a JSON data set located at <a href="https://data.baltimorecity.gov/Culture-Arts/Libraries/tgtv-wr5u">https://data.baltimorecity.gov/Culture-Arts/Libraries/tgtv-wr5u</a>
+    This web app finds and maps the location of every Baltimore city public library listed on the Enoch Pratt Library website.
+</p><p>
+    This was inspired by a public JSON data set located at <a href="https://data.baltimorecity.gov/Culture-Arts/Libraries/tgtv-wr5u">https://data.baltimorecity.gov/Culture-Arts/Libraries/tgtv-wr5u</a>. Unfortunately the file was incomplete and could have been organized better, so I went through the Pratt Library website to collect data and created my own JSON file to reference.
 </p>
-<div id="map" style="height:400px; width: 400px;"></div>
-<br><br>
+<div id="map"></div>
+<?php
+
+// grab file contents in php
+$library_json = file_get_contents('libraries.json');
+$libraries = json_decode( $library_json );
+$zip_codes = [];
+
+// get zip codes
+foreach( $libraries as $library ) {
+    array_push( $zip_codes, $library->address->zipCode );
+}
+
+// remove duplicates
+$zip_codes = array_unique($zip_codes);
+
+// sort by ascending order
+sort($zip_codes);
+
+?>
 <form id="form">
     <select>
-        <option value="21201">21201</option>
-        <option value="21225">21225</option>
-        <option value="21224">21224</option>
-        <option value="21225">21225</option>
-        <option value="21213">21213</option>
-        <option value="21229">21229</option>
-        <option value="21216">21216</option>
-        <option value="21212">21212</option>
-        <option value="21214">21214</option>
-        <option value="21211">21211</option>
-        <option value="21213">21213</option>
-        <option value="21230">21230</option>
-        <option value="21218">21218</option>
-        <option value="21224">21224</option>
-        <option value="21217">21217</option>
-        <option value="21215">21215</option>
-        <option value="21210">21210</option>
-        <option value="21216">21216</option>
-        <option value="21230">21230</option>
-        <option value="21218">21218</option>
-        <option value="21224">21224</option>
-        <option value="21231">21231</option>
+        <?php foreach ( $zip_codes as $zip ) {
+            echo '<option value="' . $zip . '">' . $zip . '</option>';
+        } ?>
     </select>
     <input type="submit" value="Submit">
 </form>
@@ -63,27 +85,50 @@
 
             initMap();
 
-            var zipcode = $(this).find(':selected').val();
+            var zip_code = $(this).find(':selected').val();
 
-            query_libraries( zipcode );
+            query_libraries( zip_code );
         });
 
-        function query_libraries( zipcode ) {
+        function query_libraries( zip_code ) {
 
-            $.getJSON( "libraries.json", function( data ) {
+            $.getJSON( 'libraries.json', function( data ) {
 
-                jQuery.each( data['data'], function() {
+                //console.log(data);
 
-                    if ( this[9] == zipcode ) {
+                for( var key in data ) {
 
-                        var address_json = JSON.parse(this[13][0]),
-                            address = address_json.address + ', '
-                                + address_json.city + ', '
-                                + address_json.state + ', '
-                                + this[9];
+                    // object iteration
+                    var obj = data[key];
 
-                        var library_location = '<div style="margin-bottom:20px">' + this[8] + ' Library' + ', ' + this[10] + '<br>' + address + '</div>';
-                        $(library_location).appendTo('#results');
+                    var address_obj = obj['address'];
+
+                    if ( address_obj.zipCode == zip_code ) {
+
+                        var address = address_obj.streetName + '<br>' + address_obj.city + ', ' + address_obj.state + ', ' + address_obj.zipCode;
+
+                        var name = obj['locationName'],
+                            phone = obj['phoneNumber'],
+                            email = obj['contactEmail'],
+                            website = obj['website'];
+
+                        var hours_obj = obj['hoursOfOperation'],
+                            hours_div = '';
+
+                        $(hours_obj).each( function(){
+                            hours_div  += '<div>' + this.day + ': ' + this.hours + '</div>';
+                        });
+
+                        var results = '<div class="location">'
+                            + '<strong>' + name + '</strong><br>'
+                            + address + '<br>'
+                            + phone + '<br>'
+                            + email + '<br>'
+                            + '<a href="' + website + '" target="_blank">' + website + '</a>'
+                            //+ hours_div
+                            + '</div>';
+
+                        $(results).appendTo('#results');
 
                         geocoder.geocode( { 'address': address}, function(results, status) {
 
@@ -100,8 +145,9 @@
                                 alert('Geocode was not successful for the following reason: ' + status);
                             }
                         });
+
                     }
-                });
+                }
             });
         }
     });
